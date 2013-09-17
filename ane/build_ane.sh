1915075d74f63230a1ca58d9e42ac690909a3a5d
soywiz <soywiz@gmail.com>
2013-09-18 00:41:32 +0200

    WIP Mac

--- ane/build_ane.sh
+++ ane/build_ane.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-rm library.swf 2> /dev/null
-rm libwebp_ane.swc 2> /dev/null
-rm ..\bin\libwebp.ane 2> /dev/null
+rm library.swf
+rm libwebp_ane.swc
+rm ../bin/libwebp.ane
 /opt/airsdk/bin/acompc -source-path as3 -include-sources as3 -optimize -swf-version 13 -output libwebp_ane.swc
-7za -y e libwebp_ane.swc library.swf 2> /dev/null
-cp -y library.swf windows\library.swf 2> /dev/null
-cp -y library.swf android\library.swf 2> /dev/null
-cp -y library.swf default\library.swf 2> /dev/null
-rm library.swf 2> /dev/null
+7za -y e libwebp_ane.swc library.swf
+cp -f library.swf windows/library.swf
+cp -f library.swf mac/library.swf
+cp -f library.swf android/library.swf
+cp -f library.swf default/library.swf
+rm library.swf
 /opt/airsdk/bin/adt -package -target ane ..\bin\libwebp.ane extension.xml -swc libwebp_ane.swc \
 	-platform Android-ARM -C android . \
 	-platform Windows-x86 -C windows . \
+	-platform MacOS-x86 -C mac . \
 	-platform default -C default .
-DEL libwebp_ane.swc 2> /dev/null
+rm libwebp_ane.swc
--- ane/webp_extension.c
+++ ane/webp_extension.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../libwebp/src/webp/decode.h"
 #include <FlashRuntimeExtensions.h>
 
