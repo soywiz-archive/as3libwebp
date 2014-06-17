d07cf66bf043f7f386ff053e5ee49d6412d7b1e3
Carlos Ballesteros Velasco <soywiz@users.noreply.github.com>
2014-06-17 10:38:40 +0200

    Update build_ane.bat

--- ane/build_ane.bat
+++ ane/build_ane.bat
@@ -1,10 +1,11 @@
 @ECHO OFF
 
 SET AIRSDK=c:\dev\airsdk
+SET NAME=libwebp
 
 REM SET SIGNING_OPTIONS=-storetype pkcs12 -keystore test.p12 -storepass test
-REM "%AIRSDK%\bin\adt.bat" -package -storetype pkcs12 -keystore test.p12 -storepass test -target ane libwebp.ane extension.xml -swc libwebp_ane.swc -platform Windows-x86 library.swf libwebp_ane.dll
-REM "%AIRSDK%\bin\adt.bat" -package %SIGNING_OPTIONS% -target ane libwebp.ane extension.xml
+REM "%AIRSDK%\bin\adt.bat" -package -storetype pkcs12 -keystore test.p12 -storepass test -target ane %NAME%.ane extension.xml -swc %NAME%_ane.swc -platform Windows-x86 library.swf %NAME%_ane.dll
+REM "%AIRSDK%\bin\adt.bat" -package %SIGNING_OPTIONS% -target ane %NAME%.ane extension.xml
 
 SET PLATFORMS=
 REM SET PLATFORMS=%PLATFORMS% -platformoptions platformoptions.xml
@@ -15,14 +16,14 @@ SET PLATFORMS=%PLATFORMS% -platform Windows-x86 -C windows .
 SET PLATFORMS=%PLATFORMS% -platform default -C default .
 
 DEL library.swf 2> NUL
-DEL libwebp_ane.swc 2> NUL
-DEL ..\bin\libwebp.ane 2> NUL
-CALL "%AIRSDK%\bin\acompc" -source-path as3 -include-sources as3 -optimize -swf-version 13 -output libwebp_ane.swc
-..\utils\7z -y e libwebp_ane.swc library.swf 2> NUL > NUL
+DEL %NAME%_ane.swc 2> NUL
+DEL ..\bin\%NAME%.ane 2> NUL
+CALL "%AIRSDK%\bin\acompc" -source-path as3 -include-sources as3 -optimize -swf-version 13 -output %NAME%_ane.swc
+..\utils\7z -y e %NAME%_ane.swc library.swf 2> NUL > NUL
 COPY /y library.swf windows\library.swf 2> NUL
 COPY /y library.swf android\library.swf 2> NUL
 COPY /y library.swf default\library.swf 2> NUL
 DEL library.swf 2> NUL
-REM CALL "%AIRSDK%\bin\adt" -package -target ane libwebp.ane extension.xml -swc libwebp_ane.swc %PLATFORMS%
-CALL "%AIRSDK%\bin\adt" -package -target ane ..\bin\libwebp.ane extension.xml -swc libwebp_ane.swc %PLATFORMS%
-DEL libwebp_ane.swc 2> NUL
+REM CALL "%AIRSDK%\bin\adt" -package -target ane %NAME%.ane extension.xml -swc %NAME%_ane.swc %PLATFORMS%
+CALL "%AIRSDK%\bin\adt" -package -target ane ..\bin\%NAME%.ane extension.xml -swc %NAME%_ane.swc %PLATFORMS%
+DEL %NAME%_ane.swc 2> NUL
